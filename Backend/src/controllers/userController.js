d960c0bea42cd0db92638c227705a41d1266e87f
betelhemze <betelhemwondale@gmail.com>
2026-03-22 14:29:54 +0300

    added logout route and controller

--- Backend/src/controllers/userController.js
+++ Backend/src/controllers/userController.js
@@ -59,5 +59,20 @@ const loginUser = async (req,res) => {
     }catch(error){
         return res.status(500).json({message:"Internal server error", error: error.message});
     }
-}
- export {createUser, loginUser}
+};
+const logoutUser = async (req, res) => {
+    try {
+        const {email} =req.body; //get email from request body 
+        const user = await User.findOne({ 
+            email: email.toLowerCase()
+        });
+        if (!user){
+            return res.status(400).json({message: "user does not exist"});
+        }
+        res.status(200).json({message: "logout successful"});
+    }catch (error)
+
+   { return res.status(500).json({message: "Internal server error", error: error.message});
+   }
+};
+ export {createUser, loginUser, logoutUser}
--- Backend/src/routes/userRoutes.js
+++ Backend/src/routes/userRoutes.js
@@ -1,9 +1,10 @@
 import {Router} from "express";
 import {createUser} from "../controllers/userController.js"
-import {loginUser} from "../controllers/userController.js"
+import { loginUser, logoutUser } from "../controllers/userController.js";
+
 const router = Router();
 
 router.post("/create", createUser);
 router.post("/login", loginUser);
-
+router.post("/logout", logoutUser )
 export default router;
